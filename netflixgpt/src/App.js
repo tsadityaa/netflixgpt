c6e07ebd59ea193b62f5fa987c0e0cce8f91b3ed
tsadityaa <t.s.aditya34@gmail.com>
2024-09-30 00:27:09 +0530

    Initial commit for NetflixGPT

---
+++ netflixgpt/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>fuck</h1>
+        <h2>thank you much fucking i was wasting so much time on this</h2>
+    </div>
+  );
+}
+
+export default App;
